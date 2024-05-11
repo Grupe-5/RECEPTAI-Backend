e576167e96d6fe5a02ff257eec67145a118f8f92
domaskal <domaskal@gmail.com>
2024-05-12 00:11:55 +0300

    feat: user info retrieve & user deletion

---
+++ receptai.api/Mappers/UserMappers.cs
@@ -0,0 +1,20 @@
+﻿using receptai.data;
+
+namespace receptai.api;
+
+public static class UserMappers
+{
+    public static UserInfoDto ToUserInfoDto(
+        this User userModel)
+    {
+        return new UserInfoDto
+        {
+            Id = userModel.Id,
+            Username = userModel.UserName!,
+            JoinDate = userModel.JoinDate,
+            ImageId = userModel.ImgId,
+            /* This prob needs to be dynamically accessed */
+            KarmaScore = userModel.KarmaScore,
+        };
+    }
+}
