a0304f6f65a1ec25a5274facb9f8add95e502717
domaskal <domaskal@gmail.com>
2024-05-12 00:10:38 +0300

    feat: image controller, service, repo

---
+++ receptai.api/Controllers/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace receptai.api.Controllers;
+
+[Route("api/image")]
+[ApiController]
+public class ImageController(IImageService imageService) : ControllerBase
+{
+    private readonly IImageService _imageService = imageService;
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var bytes = await _imageService.FetchImageAsync(id);
+        if (bytes == null)
+        {
+            return NotFound();
+        }
+
+        return File(bytes, "image/jpeg");
+    }
+}
---
+++ receptai.api/Repositories/ImageRepository.cs
@@ -0,0 +1,42 @@
+﻿
+using receptai.data;
+
+namespace receptai.api;
+
+public class ImageRepository(RecipePlatformDbContext context) : IImageRepository
+{
+    private readonly RecipePlatformDbContext _context = context;
+
+    public async Task<bool> DeleteImageAsync(int imageId)
+    {
+        var img = await _context.Images.FindAsync(imageId);
+        if (img == null)
+        {
+            return false;
+        }
+        else
+        {
+            _context.Images.Remove(img);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+
+    public async Task<byte[]?> RetrieveImageAsync(int imageId)
+    {
+        var img = await _context.Images.FindAsync(imageId);
+        return img?.ImageData;
+    }
+
+    public async Task<int> UploadImageAsync(byte[] bytes)
+    {
+        Image img = new()
+        {
+            ImgId = 0,
+            ImageData = bytes,
+        };
+        var ent = await _context.Images.AddAsync(img);
+        await _context.SaveChangesAsync();
+        return ent.Entity.ImgId;
+    }
+}
